e40751b68a906826e53c503de55735e16b485b4e
Tüzes Dániel otthonról <tuzesdaniel@gmail.com>
2021-03-10 17:08:09 +0000

    _teefile and _sourcefile implemented and tested

--- namedPrng/examples.py
+++ namedPrng/examples.py
@@ -3,37 +3,57 @@
 
 import named_prng
 
+# quarks is a particle type with 6 different IDs as keys
+# The order number will be important once we remove IDs.
 quarks = {"up": 0, "down": 1, "charm": 2, "strange": 3, "top": 4, "bottom": 5}
+
+# atoms is another particle type with 4 different IDs as keys.
+# We wont't use them btw.
 atoms = {"H": 0, "He": 1, "Li": 2, "Be": 3}
 
+# the dict of dict, all particles together,
+# and we also know the name of their type
 mparticles = {"quarks": quarks, "atoms": atoms}
 
-Mnprng = named_prng.NamedPrng(mparticles, teefile="tee.dat")
+# I will generate random numbers for a smaller set of IDs too
+remove_quarks = {"charm", "bottom"}
+quarks_subset = quarks.copy()  # contains the subset of quarks
+for rid in remove_quarks:
+    quarks_subset.pop(rid)
 
-for realization_id in range(3):
-    print("realization_id = ", realization_id)
-    # generate random numbers for quarks
-    Mnprng.init_prngs(realization_id)
-    random_for_quarks = Mnprng.normal("quarks")
 
-    for key, rnd in zip(quarks, random_for_quarks):
-        print(key, rnd)
+def do_some_stuff(mnprng: named_prng.NamedPrng) -> None:
+    """Do some stuff with the NamedPrng.
+        I will call it without and with _sourcefile defined to see
+        if it gives the same result."""
+    for realization_id in range(2):
+        print("realization_id = ", realization_id)
 
-    # generate random numbers for a subset of quarks
-    remove_IDs = {"charm", "bottom"}
+        # generate random numbers for quarks
+        mnprng.init_prngs(realization_id)
+        random_for_quarks = mnprng.normal("quarks")
 
-    quarks_constr = quarks.copy()  # contains the subset of quarks, just for printing
-    for ID in remove_IDs:
-        quarks_constr.pop(ID)
-    print("------------------ now for the restricted set of particles ------------------")
+        for key, rnd in zip(quarks, random_for_quarks):
+            print(key, rnd)
 
-    Mnprng.init_prngs(realization_id)    # reset the prngs
+        # generate random numbers for a subset of quarks
+        print("--------------- now for the restricted set of particles ---------------")
 
-    random_for_quarks_constr = Mnprng.normal("quarks", exclude_ids=remove_IDs)
+        mnprng.init_prngs(realization_id)    # reset the prngs
 
-    for key, rnd in zip(quarks_constr, random_for_quarks_constr):
-        print(key, rnd)
-    print("=============================================================================")
+        random_for_quarks_subset = mnprng.normal(
+            "quarks", exclude_ids=remove_quarks)
 
-print("All the random numbers generated so far:")
+        for key, rnd in zip(quarks_subset, random_for_quarks_subset):
+            print(key, rnd)
+        print("=======================================================================")
+
+
+Mnprng_gen = named_prng.NamedPrng(mparticles, teefilename="tee.dat")
+do_some_stuff(Mnprng_gen)
+
+Mnprng_use = named_prng.NamedPrng(mparticles, sourcefilename="tee.dat")
+do_some_stuff(Mnprng_use)
+
+print("All the random numbers in the tee file:")
 print(named_prng.NamedPrng.get_rnds_from_file("tee.dat"))
--- namedPrng/named_prng.py
+++ namedPrng/named_prng.py
@@ -1,7 +1,7 @@
 """named_prng.py
     This file contains the implementation of the
     named pseudo random number generator. Detailed documentation is available
-    in the README.md file too."""
+    in the README.md file too. Check out examples.py for examples!"""
 
 import sys
 from typing import Dict, Iterable
@@ -24,9 +24,16 @@ class NamedPrng:
             and the order number of particles as the values.
         _ptype_ind: Dict[str,int]
             The order indices of the particle types.
-        _teefile: str
         _engines: Dict[str, numpy.random.Generator]
-
+        _teefile: BinaryIO
+            If set, all random numbers generated are copied to this file.
+            The file is opened with the initializator and closed once
+            the instance goes out of scope.
+        _sourcefile: BinaryIO
+            If set, numbers from this file is read instead of generating them
+            with the numpy random generator (e.g. Mersenne Twister). It is
+            the user's responsibility to make sure the file has enough random
+            numbers and that these numbers have the required properties.
         Methods
         -----------
         _seed_map(self, realization: int, ptype: str) -> int:
@@ -39,17 +46,42 @@ class NamedPrng:
 
     def __init__(self,
                  particles: Dict[str, Dict[str, int]],
-                 teefile: str = None,
-                 realization_id: int = None):
-
+                 teefilename: str = None,
+                 realization_id: int = None,
+                 sourcefilename: str = None):
         self._particles = particles
-        if len(particles) > self.N_max:
+        if len(particles) > self.N_max:  # sanity check
             sys.exit("The prng is fed with", len(particles), "number of types,",
                      "but only", self.N_max,
                      "different types are supported. Program terminates.")
 
         self._ptype_ind = {key: index for index, key in enumerate(particles)}
-        self._teefile = teefile  # where to copy the generated prn-s
+        if teefilename is None:  # where to copy the generated prn-s
+            self._teefile = None
+        else:
+            try:
+                self._teefile = open(teefilename, "ab")
+            except OSError as err:
+                print("Cannot initialize a NamedPrng instance,",
+                      "because teefilename is set but OSError occured",
+                      "while opening the file for binary appending.",
+                      "No _teefile will be used.",
+                      "Error details:", err)
+                self._teefile = None
+
+        if sourcefilename is None:  # from where to read the random numbers
+            self._sourcefile = None
+        else:
+            try:
+                self._sourcefile = open(sourcefilename, "rb")
+            except OSError as err:
+                print("Cannot initialize a NamedPrng instance,",
+                      "because sourcefilename is set but OSError occured",
+                      "while opening the file for binary reading.",
+                      "prng will be used instead of the _sourcefile.",
+                      "Error details:", err)
+                self._sourcefile = None
+
         self._engines = dict()   # the prng instances
 
         if realization_id is not None:
@@ -80,7 +112,8 @@ class NamedPrng:
     def random(self,
                ptype: str,
                exclude_ids: Iterable = None) -> numpy.ndarray:
-        """Returns random numbers with uniform distribution on [0,1)
+        """If _sourcefile is not set,
+            returns random numbers with uniform distribution on [0,1)
             for each particle with type ptype that does not have the
             ID listed in excludeIDs.
 
@@ -91,9 +124,21 @@ class NamedPrng:
             excludeIDs tells the IDs for which particles the random numbers
             should be omitted from the return value. The order number of the
             random numbers are read from the value of the correcponding ID key
-            of the particles."""
-        ret = self._engines[ptype].random(len(self._particles[ptype]))
-        ret = self._tee(ret)  # copy the random numbers if needed
+            of the particles.
+
+            If _sourcefile is set, reads in 64-bit floats from _sourcefile
+            and does not modify the state of the prng instance."""
+
+        amount = len(self._particles[ptype])
+        ret = numpy.ndarray(amount, dtype=numpy.float64)
+
+        if self._sourcefile is None:
+            ret = self._engines[ptype].random(amount)
+            ret = self._tee(ret)  # copy the random numbers if needed
+        else:
+            ret = numpy.fromfile(
+                self._sourcefile, dtype=numpy.float64, count=amount)
+
         if exclude_ids is not None:
             ret = self._exclude_ids(ret, ptype, exclude_ids)
         return ret
@@ -102,7 +147,8 @@ class NamedPrng:
                exclude_ids: Iterable = None,
                loc: float = 0,
                scale: float = 1) -> numpy.ndarray:
-        """Returns random numbers with a normal (aka Gaussian) distribution
+        """If _sourcefile is not set,
+            returns random numbers with a normal (aka Gaussian) distribution
             with the properties passed for each of the particles ptype
             that does not have the ID listed in excludeIDs.
 
@@ -113,10 +159,22 @@ class NamedPrng:
             excludeIDs tells the IDs for which particles the random numbers
             should be omitted from the return value. The order number of the
             random numbers are read from the value of the correcponding ID key
-            of the particles."""
-        ret = self._engines[ptype].normal(
-            loc=loc, scale=scale, size=len(self._particles[ptype]))
-        ret = self._tee(ret)  # copy the random numbers if needed
+            of the particles.
+
+            If _sourcefile is set, reads in 64-bit floats from _sourcefile
+            and does not modify the state of the prng instance."""
+
+        amount = len(self._particles[ptype])
+        ret = numpy.ndarray(amount, dtype=numpy.float64)
+
+        if self._sourcefile is None:
+            ret = self._engines[ptype].normal(
+                loc=loc, scale=scale, size=amount)
+            ret = self._tee(ret)  # copy the random numbers if needed
+        else:
+            ret = numpy.fromfile(
+                self._sourcefile, dtype=numpy.float64, count=amount)
+
         if exclude_ids is not None:
             ret = self._exclude_ids(ret, ptype, exclude_ids)
         return ret
@@ -124,13 +182,18 @@ class NamedPrng:
     def _tee(self, arr: numpy.ndarray) -> numpy.ndarray:
         """Copy the input array arr to _teefile if exists and return the array."""
         if self._teefile is not None:
-            with open(self._teefile, "ab") as ofile:
-                arr.tofile(ofile)
+            arr.tofile(self._teefile)
         return arr
 
     @classmethod
-    def get_rnds_from_file(cls, teefile: str) -> numpy.ndarray:
-        """Returns the value in a file where random numbers are copied."""
-        with open(teefile, "rb") as ifile:
-            ret = numpy.fromfile(ifile, dtype=numpy.float64)
-        return ret
+    def get_rnds_from_file(cls, teefilename: str) -> numpy.ndarray:
+        """Returns 64-bit float values from a file
+        where random numbers are supposed to be stored."""
+        try:
+            with open(teefilename, "rb") as ifile:
+                ret = numpy.fromfile(ifile, dtype=numpy.float64)
+                return ret
+        except OSError as err:
+            print("Cannot show the random numbers from the file,",
+                  "because an OSError occured:", err)
+            return numpy.ndarray(0)
